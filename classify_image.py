f674aa474cf885b1d03fc256b113be1c1c637421
Mitra Thakker <mitra_thakker@yahoo.com>
2018-03-19 00:20:42 +0530

    Add files via upload

--- classify_image.py
+++ classify_image.py
@@ -4,7 +4,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+#         http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -24,88 +24,87 @@ import tensorflow as tf
 
 
 def load_graph(model_file):
-  graph = tf.Graph()
-  graph_def = tf.GraphDef()
+	graph = tf.Graph()
+	graph_def = tf.GraphDef()
 
-  with open(model_file, "rb") as f:
-    graph_def.ParseFromString(f.read())
-  with graph.as_default():
-    tf.import_graph_def(graph_def)
+	with open(model_file, "rb") as f:
+		graph_def.ParseFromString(f.read())
+	with graph.as_default():
+		tf.import_graph_def(graph_def)
 
-  return graph
+	return graph
 
 
 def read_tensor_from_image_file(file_name,
-                                input_height=299,
-                                input_width=299,
-                                input_mean=0,
-                                input_std=255):
-  input_name = "file_reader"
-  output_name = "normalized"
-  file_reader = tf.read_file(file_name, input_name)
-  if file_name.endswith(".png"):
-    image_reader = tf.image.decode_png(
-        file_reader, channels=3, name="png_reader")
-  elif file_name.endswith(".gif"):
-    image_reader = tf.squeeze(
-        tf.image.decode_gif(file_reader, name="gif_reader"))
-  elif file_name.endswith(".bmp"):
-    image_reader = tf.image.decode_bmp(file_reader, name="bmp_reader")
-  else:
-    image_reader = tf.image.decode_jpeg(
-        file_reader, channels=3, name="jpeg_reader")
-  float_caster = tf.cast(image_reader, tf.float32)
-  dims_expander = tf.expand_dims(float_caster, 0)
-  resized = tf.image.resize_bilinear(dims_expander, [input_height, input_width])
-  normalized = tf.divide(tf.subtract(resized, [input_mean]), [input_std])
-  sess = tf.Session()
-  result = sess.run(normalized)
+								input_height=299,
+								input_width=299,
+								input_mean=0,
+								input_std=255):
+	input_name = "file_reader"
+	output_name = "normalized"
+	file_reader = tf.read_file(file_name, input_name)
+	if file_name.endswith(".png"):
+		image_reader = tf.image.decode_png(
+			file_reader, channels=3, name="png_reader")
+	elif file_name.endswith(".gif"):
+		image_reader = tf.squeeze(
+			tf.image.decode_gif(file_reader, name="gif_reader"))
+	elif file_name.endswith(".bmp"):
+		image_reader = tf.image.decode_bmp(file_reader, name="bmp_reader")
+	else:
+		image_reader = tf.image.decode_jpeg(
+			file_reader, channels=3, name="jpeg_reader")
+	float_caster = tf.cast(image_reader, tf.float32)
+	dims_expander = tf.expand_dims(float_caster, 0)
+	resized = tf.image.resize_bilinear(dims_expander, [input_height, input_width])
+	normalized = tf.divide(tf.subtract(resized, [input_mean]), [input_std])
+	sess = tf.Session()
+	result = sess.run(normalized)
 
-  return result
+	return result
 
 
 def load_labels(label_file):
-  label = []
-  proto_as_ascii_lines = tf.gfile.GFile(label_file).readlines()
-  for l in proto_as_ascii_lines:
-    label.append(l.rstrip())
-  return label
+	label = []
+	proto_as_ascii_lines = tf.gfile.GFile(label_file).readlines()
+	for l in proto_as_ascii_lines:
+		label.append(l.rstrip())
+	return label
 
 
 def predict(file_name):
-  #file_name = "/home/ananya/trainingimages/bird/AN00282487_001_l.jpg"
-  model_file = "/home/ananya/ananya/output_graph.pb" #route of .pb file
-  label_file = "/home/ananya/ananya/output_labels.txt" #route of output_labels.txt file
-  input_height = 299
-  input_width = 299
-  input_mean = 0
-  input_std = 255
-  input_layer = "Mul"
-  output_layer = "final_result"
+	# file_name = "/home/ananya/trainingimages/bird/AN00282487_001_l.jpg"
+	model_file = "/Users/mitrathakker/Downloads/output_graph.pb"  # route of .pb file
+	label_file = "/Users/mitrathakker/Downloads/output_labels.txt"  # route of output_labels.txt file
+	input_height = 299
+	input_width = 299
+	input_mean = 0
+	input_std = 255
+	input_layer = "Mul"
+	output_layer = "final_result"
 
-  
-  graph = load_graph(model_file)
-  t = read_tensor_from_image_file(
-      file_name,
-      input_height=input_height,
-      input_width=input_width,
-      input_mean=input_mean,
-      input_std=input_std)
+	graph = load_graph(model_file)
+	t = read_tensor_from_image_file(
+		file_name,
+		input_height=input_height,
+		input_width=input_width,
+		input_mean=input_mean,
+		input_std=input_std)
 
-  input_name = "import/" + input_layer
-  output_name = "import/" + output_layer
-  input_operation = graph.get_operation_by_name(input_name)
-  output_operation = graph.get_operation_by_name(output_name)
+	input_name = "import/" + input_layer
+	output_name = "import/" + output_layer
+	input_operation = graph.get_operation_by_name(input_name)
+	output_operation = graph.get_operation_by_name(output_name)
 
-  with tf.Session(graph=graph) as sess:
-    results = sess.run(output_operation.outputs[0], {
-        input_operation.outputs[0]: t
-    })
-  results = np.squeeze(results)
+	with tf.Session(graph=graph) as sess:
+		results = sess.run(output_operation.outputs[0], {
+			input_operation.outputs[0]: t
+		})
+	results = np.squeeze(results)
 
-  top_k = results.argsort()[-5:][::-1]
-  labels = load_labels(label_file)
-  finaloutput = {}
-  for i in top_k:
-    finaloutput[labels[i]] = results[i]
-   return finaloutput
+	top_k = results.argsort()[-5:][::-1]
+	labels = load_labels(label_file)
+	finaloutput = {}
+	for i in top_k:
+		finaloutput[labels[i]] = results[i]
+	return finaloutput
--- codingculture.py
+++ codingculture.py
@@ -2,7 +2,9 @@ import os
 
 from flask import Flask, render_template, request
 from flask_dropzone import Dropzone
-from mitra.src.utils.neo4j_utils import *
+from mitra.src.utils.neo4j_utils import get_graph_connection, create_image_node, create_image_tag_relationship
+from mitra.src.utils.image_utils import get_image_hash
+from classify_image import predict
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -10,10 +12,10 @@ app = Flask(__name__)
 conn = get_graph_connection(host="localhost", port="7474", user_name="neo4j", password="neo")
 
 app.config.update(
-    UPLOADED_PATH=os.path.join(basedir, 'uploads'),
-    DROPZONE_ALLOWED_FILE_TYPE='image',
-    DROPZONE_MAX_FILE_SIZE=3,
-    DROPZONE_MAX_FILES=30,
+	UPLOADED_PATH=os.path.join(basedir, 'uploads'),
+	DROPZONE_ALLOWED_FILE_TYPE='image',
+	DROPZONE_MAX_FILE_SIZE=3,
+	DROPZONE_MAX_FILES=30,
 )
 
 drop_zone = Dropzone(app)
@@ -21,11 +23,15 @@ drop_zone = Dropzone(app)
 
 @app.route('/', methods=['POST', 'GET'])
 def upload():
-    if request.method == 'POST':
-        f = request.files.get('file')
-        f.save(os.path.join(app.config['UPLOADED_PATH'], f.filename))
-    return render_template('index.html')
+	if request.method == 'POST':
+		f = request.files.get('file')
+		file_path = os.path.join(app.config['UPLOADED_PATH'], f.filename)
+		f.save(file_path)
+		print(predict(file_path))
+		# image_node = create_image_node(name=f.filename, image_hash=get_image_hash(image_url=file_path))
+	# 	This is where you pass the file path for classification
+	return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+	app.run(debug=True)
